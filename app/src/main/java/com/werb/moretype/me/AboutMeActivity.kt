a5c0ff54f197758a2472de27f3d8ee933acabe32
wanbo <wanbowb@live.com>
2017-07-17 14:10:34 +0800

    add shortcuts

--- app/src/main/java/com/werb/moretype/me/AboutMeActivity.kt
+++ app/src/main/java/com/werb/moretype/me/AboutMeActivity.kt
@@ -1,5 +1,6 @@
 package com.werb.moretype.me
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
@@ -7,6 +8,7 @@ import com.werb.library.MoreAdapter
 import com.werb.moretype.R
 import com.werb.moretype.TitleViewType
 import com.werb.moretype.data.DataServer
+import com.werb.moretype.main.MainActivity
 import kotlinx.android.synthetic.main.activity_me.*
 
 /**
@@ -15,13 +17,16 @@ import kotlinx.android.synthetic.main.activity_me.*
 class AboutMeActivity: AppCompatActivity() {
 
     private val adapter = MoreAdapter()
+    private var short = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_me)
 
+        short = intent.action == "shortStart"
+
         toolbar.setNavigationIcon(R.mipmap.ic_close_white_24dp)
-        toolbar.setNavigationOnClickListener { finish() }
+        toolbar.setNavigationOnClickListener { onBackPressed() }
 
         me_list.layoutManager = LinearLayoutManager(this)
         adapter.userSoleRegister()
@@ -36,6 +41,15 @@ class AboutMeActivity: AppCompatActivity() {
 
         adapter.loadData(DataServer.getAboutMeData())
         me_list.smoothScrollToPosition(0)
+    }
+
+    override fun onBackPressed() {
+        if(!short){
+            finish()
+        } else {
+            startActivity(Intent(AboutMeActivity@this, MainActivity::class.java))
+            finish()
+        }
     }
 
 }
